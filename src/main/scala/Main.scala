cf42efe494351170f48cef1c4aa09b50b8e8c7df
igfe <ignferr@gmail.com>
2022-07-19 12:53:03 +0200

    add gitignore

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,20 +1,32 @@
 import cilib._
+import cilib.exec._
+import cilib.pso.Defaults._
+import cilib.pso._
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.auto._
+import eu.timepit.refined.collection.NonEmpty
+import eu.timepit.refined.numeric._
+import scalaz.effect._
+import scalaz.stream.{Process, _}
+import spire.implicits._
+import spire.math.Interval
 
-object Main {
-  def r_var_test(rng: RNG): Unit = {
-    val listOfInts = RVar.ints(12)
+object Main extends SafeApp {
+  // General config
+  val iterations = 100
+  val independentRuns = 2
+  val cores = 8
+  val outputFile = "results.csv"
+  val numberOfParticles: Int Refined Positive = 10
+  val bounds = Interval(-5.12, 5.12) ^ 30 // The bounds of our search space(s)
 
-    val doubledListOfInts =
-      for {
-        list <- listOfInts
-      } yield list.map(x => x * 2)
+  // Our algorithm
+  val cognitive = Guide.pbest[Mem[Double], Double]
+  val social = Guide.gbest[Mem[Double]]
+  val gbestPSO = gbest(0.729844, 1.496180, 1.496180, cognitive, social)
+  val algorithmName: String Refined NonEmpty = "GBest PSO"
+  val swarm = Position.createCollection(PSO.createParticle(x => Entity(Mem(x, x.zeroed), x)))(bounds, numberOfParticles)
 
-    val result = doubledListOfInts.run(rng)
-    println(result)
-  }
 
-  def main(args: Array[String]): Unit = {
-    val rng = RNG.init(1234)
-    r_var_test(rng)
-  }
+
 }
