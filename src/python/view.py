3ecd9bfa55d88e32009e16942a35ce870b32a21f
igfe <ignferr@gmail.com>
2022-08-08 16:23:12 +0200

    modularized view.py

--- src/python/view.py
+++ src/python/view.py
@@ -2,8 +2,22 @@ import pyarrow.parquet as pq
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-def plot_from_df(df, column):
+"""
+	TODO:
+		add titles and labels to the graphs from parquet entries
+		separate extract_data from plot_column 
+		use pq or pd to merge data instead of looping over iterations
+"""
+def get_pq_files(directory):
+	extension = ".parquet"
+	files = [f for f in os.listdir(directory) if f.endswith(extension)]
+	paths = [os.path.join(directory, f) for f in files]
+	print(paths)
+	return paths
+
+def plot_column(df, column):
 	print(f"with {column=}")
 	xs = []
 	for i in df["iteration"].unique():
@@ -11,15 +25,23 @@ def plot_from_df(df, column):
 		xs += [sum(v)/len(v)]
 	plt.plot(xs, label=column)
 
-print("reading parquet table")
-table = pq.read_table('out/results.parquet')
-print("parquet to pandas")
-df = table.to_pandas()
+def read_pq_as_df(path):
+	print("reading parquet table")
+	table = pq.read_table(path)
+	print("parquet to pandas")
+	df = table.to_pandas()
+	return df
 
-plot_from_df(df, "min")
-plot_from_df(df, "average")
+def plot_from_df(df):
+	plot_column(df, "min")
+	plot_column(df, "average")
 
-print("plotting")
-plt.yscale('symlog')
-plt.legend()
-plt.show()
+	print("plotting")
+	plt.yscale('symlog')
+	plt.legend()
+	plt.show()
+
+if __name__ == '__main__':
+	paths = get_pq_files("out/")
+	df = read_pq_as_df(paths[0])
+	plot_from_df(df)
